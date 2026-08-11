8d9794df7bbbfa5dce16789a67838f878639170f
Siran <simranhaidar91@gmail.com>
2026-08-11 19:18:14 +0530

    fix : Console error

--- client/src/index.js
+++ client/src/index.js
@@ -7,8 +7,6 @@ import App from './App.jsx'
 // If there's no env var, we provide a placeholder client ID to allow the UI to render.
 const GOOGLE_CLIENT_ID = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
-console.log("Google Client ID:", GOOGLE_CLIENT_ID);
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
